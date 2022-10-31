3130c43e760f6d5e0132f889062dd46f45f83634
MustafaaYildirim <myildirim3710@gmail.com>
2022-10-31 15:53:58 +0300

    deneme4

---
+++ chapter_2/lesson3/sandbox.js
@@ -0,0 +1,26 @@
+//strings
+console.log('hello, world');
+let email = 'myildirim3710@gmail.com';
+console.log(email);
+
+//string concatenation
+let fisrtName = 'Mustafa';
+let lastName = 'Yıldırım';
+
+let fullName = fisrtName + ' ' + lastName;
+console.log(fullName);
+
+
+//getting characters
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
