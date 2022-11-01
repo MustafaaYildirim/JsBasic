71d3ece7822e8316b65b27b11523ba3cefbeadb4
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-01 13:23:12 +0300

    deneme5

---
+++ chapter_2/lesson4/sandbox.js
@@ -0,0 +1,12 @@
+//common string methods
+
+let email = 'myildirim3710@gmail.com'
+
+//let result = email.lastIndexOf('g');
+
+//let result = email.slice(2,5);
+
+//let result = email.substring(2,10);
+
+let result = email.replace('m', 'w');
+console.log(result);
