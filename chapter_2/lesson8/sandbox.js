26a817d6fb1fdb3d666523607a7211a6cc98a0cb
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-01 16:21:46 +0300

    lesson8

---
+++ chapter_2/lesson8/sandbox.js
@@ -0,0 +1,30 @@
+//booleans & comparisons
+console.log(true, false, "true", "false");
+
+//methods can return booleans
+//let email = 'myildirim3710@gmail.com'
+//let names = ['mustafa', 'mustaa', 'musti'];
+
+//let result = email.includes('@');
+//let result = names.includes('mustaa');
+//console.log(result);
+
+//comparison operators
+
+let age = 25;
+
+//console.log(age == 25);
+//console.log(age == 30);
+//console.log(age != 30);
+//console.log(age != 25);
+//console.log(age > 20);
+//console.log(age < 20);
+//console.log(age >= 25);
+//console.log(age <= 25);
+
+let names = 'mustafa';
+
+console.log(names == 'mustafa');
+console.log(names == 'Mustafa');
+console.log(names > 'Mustafa');
+
