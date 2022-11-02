d7abe9423a778f0f2e89bf234e058af7acb0fd23
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-02 15:46:50 +0300

    c3_lesson2

---
+++ chapter_3/lesson2/sandbox.js
@@ -0,0 +1,13 @@
+//while loops
+const names = ['mustafa', 'musti', 'mustaa'];
+i = 0;
+while (i < 5){
+    console.log('in loop:', i);
+    i++;
+}
+
+let k = 0;
+while(k < names.length){
+    console.log(names[k]);
+    k++;
+}
