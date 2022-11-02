5809853d1bbc85805452b1b987a39fd3375394d9
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-02 15:17:02 +0300

    c_3lesson1

---
+++ chapter_3/lesson1/sandbox.js
@@ -0,0 +1,14 @@
+//for loops
+
+for (let i = 0; i < 5; i++){
+    console.log('in loop:', i);
+}
+    console.log('loop finishd');
+
+    const names = ['mustafa', 'musti', 'mustaa'];
+
+    for( let k = 0; k < names.length; k++){
+        //console.log( names[k]);
+        let html = `<div>${names[k]}</div>`;
+        console.log(html);
+    }
