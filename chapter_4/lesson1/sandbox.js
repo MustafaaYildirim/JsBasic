ed061f8d2401b8c1d55849085bb7ee40fed328fe
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-04 16:14:01 +0300

    c4_lesson1

---
+++ chapter_4/lesson1/sandbox.js
@@ -0,0 +1,16 @@
+//function declaration  --- yeri önemsiz
+function greet(){
+    console.log('hello there');
+}
+greet();
+greet();
+greet();
+
+//function expression --- önce fonksiyon tanımlanmalı!
+
+const speak = function(){
+    console.log('have a good day!');
+};
+speak();
+speak();
+speak();
