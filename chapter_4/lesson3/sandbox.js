bbbbdda05770179ef808a4abe14eb0514c1ba438
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-05 20:15:23 +0300

    c4_lesson3

---
+++ chapter_4/lesson3/sandbox.js
@@ -0,0 +1,13 @@
+//returning values
+
+/*const speak = function(name = 'mustafa', time = 'night'){
+    console.log(`good ${time} ${name}`);
+};*/
+
+const calcArea = function(radius){
+    let area = 3.14 * radius**2;
+    return area;
+};
+const a = calcArea(5);
+console.log(a);
+
