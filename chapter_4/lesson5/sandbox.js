2c8f672eab185e4d0a3e4049a9e8399ebd09a9a5
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-05 20:50:50 +0300

    c4_lesson5

---
+++ chapter_4/lesson5/sandbox.js
@@ -0,0 +1,13 @@
+const names = 'mustafa';
+
+//functions
+const greet = () => 'hello';
+
+let resultOne = greet();
+console.log(resultOne);
+
+
+//methods
+
+let resultTwo = names.toUpperCase();
+console.log(resultTwo);
