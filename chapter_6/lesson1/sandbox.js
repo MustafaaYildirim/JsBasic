815ff8899af06ce27d1fe1bd7b2f115d9ee461b5
MustafaaYildirim <myildirim3710@gmail.com>
2022-11-07 18:19:25 +0300

    c6_lesson1

---
+++ chapter_6/lesson1/sandbox.js
@@ -0,0 +1,9 @@
+/*const para = document.querySelector('p');
+
+console.log(para);*/
+
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+console.log(paras);
+console.log(errors);
